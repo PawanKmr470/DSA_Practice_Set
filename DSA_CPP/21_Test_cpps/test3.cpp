ebd134785e2232b810c19f3caae7340f318c2ea2
PawanKmr470 <pawankmr470@gmail.com>
2024-12-07 01:55:03 +0530

    cpp dsa

--- DSA_CPP/21_Test_cpps/test3.cpp
+++ DSA_CPP/21_Test_cpps/test3.cpp
@@ -1,112 +1,15 @@
 #include <iostream>
 using namespace std;
 
-class Node {
-    public:
-        int data;
-        Node* next;
-        Node* prev;
-        Node() {
-            data = 0;
-            next = nullptr;
-            prev = nullptr;
-        }
-        Node(int val) {
-            data = val;
-            next = nullptr;
-            prev = nullptr;
-        }
-};
-
-class LinkedList {
-    private:
-        Node* head;
-    public:
-        LinkedList() { head = nullptr; }
-        ~LinkedList() {};
-
-        void insertBegin(int val) {
-            Node* temp = new Node(val);
-            if (head != nullptr) {
-                head->prev = temp;
-                temp->next = head;
-            }
-            head = temp;
-        }
-
-        void printSLL() {
-            if (head == nullptr) {
-                cout << "Linked list is empty." << endl;
-                return;
-            }
-            Node* cur = head;
-            while(cur != nullptr) {
-                cout << cur->data << " ";
-                cur = cur->next;
-            }
-            cout << endl;
-        }
-
-        void reverse() {
-            if (head == nullptr)
-                return;
-            
-            Node* cur = head;
-            Node* temp = nullptr;
-            // while (cur != nullptr) {
-            //     temp = cur->next;
-            //     cur->next = cur->prev;
-            //     cur->prev = temp;
-            //     cur = cur->prev;
-            // }
-            // Need to perform head = last node as cur has gone NULL
-            // and temp will also have NULL.
-            // with this approach, don't have previous node info
-            // So temp = cur->prev would have worked
-
-            while (cur != nullptr) {
-                temp = cur->prev;
-                cur->prev = cur->next;
-                cur->next = temp;
-                cur = cur->prev;
-            }
-            if (temp != nullptr)
-                head = temp->prev;
-        }
-
-        void reverseRec() {
-            if (head == nullptr)
-                return;
-            
-            Node* node = __reverseRec(head);
-            node->next = nullptr;
-            return;
-        }
-        Node* __reverseRec(Node* node) {
-            if (node->next == nullptr) {
-                head = node;
-                head->prev = nullptr;
-                return node;
-            }
-
-            Node* temp = __reverseRec(node->next);
-            temp->next = node;
-            node->prev = temp;
-            return node;
-        }
+class Test {
+public:
+    // Test() { cout << "default ctor" << endl; }
+    Test() = default;
+    Test(int x) { cout << "param ctor" << endl; }
 };
 
 int main() {
-    LinkedList l;
-    l.insertBegin(5);
-    l.insertBegin(10);
-    l.insertBegin(15);
-    l.insertBegin(20);
-    l.insertBegin(25);
-    l.printSLL();
-    l.reverse();
-    l.printSLL();
-    l.reverseRec();
-    l.printSLL();
+    Test t1;
+    Test t2(10);
     return 0;
 }
--- DSA_CPP/21_Test_cpps/test5.cpp
+++ DSA_CPP/21_Test_cpps/test5.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include <map>
 using namespace std;
 
+class HashTable {
+private:
+    vector<list<pair<string, string>>> table;
+    int table_size;
+
+    
+public:
+    HashTable(int size) : table_size(size) {
+        table.resize(size);
+    }
+
+};
+
 int main() {
 
-    vector<int> v1 {20,20,30,20,40,30,40,50,10,20,10,40,10,10,20};
-    map<int, int> freq;
-    for (auto i : v1) {
-        freq[i]++;
-    }
-
-    for (auto i : freq) {
-        cout << i.first << " " << i.second << endl;
-    }
-    cout << endl;
-
-    vector<pair<int, int>> v2(freq.begin(), freq.end());
-    sort(v2.begin(), v2.end(), [] (const pair<int, int>& a, const pair<int, int>& b) { return a.second > b.second; });
-
-    for (auto i : v2) {
-        cout << i.first << " " << i.second << endl;
-    }
-
-
-    return 0;
+    
 }
